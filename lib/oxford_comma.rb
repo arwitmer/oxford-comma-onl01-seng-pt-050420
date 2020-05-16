4235950a64d33a33d72a210b815b18692f7a29d5
imperative-tuple-4883 <andrew.witmer445@gmail.com>
2020-05-16 08:08:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,12 @@
-def oxford_comma(array)
-  array = ["kiwi", "durian", ]
-  array.join(",")
+def oxford_comma(arr)
+  case arr.size
+  when 0
+    ""
+  when 1
+    arr.first
+  when 2
+    arr.join(' and ')
+  else
+    [arr[0..-2].join(', '), arr.last].join(', and ')
+  end
 end
